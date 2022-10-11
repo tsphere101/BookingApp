03b6f4b4f3c88440d9f917c16cb1444933cbd490
Chanwanit-Nuchyoo <peaknuchyoo@gmail.com>
2022-10-11 23:28:37 +0700

    add filter bar

--- booking_app/src/components/Housekeeping/Filter bar/FilterBar.js
+++ booking_app/src/components/Housekeeping/Filter bar/FilterBar.js
@@ -3,31 +3,74 @@ import styles from "./FilterBar.module.css";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const FilterBar = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
   return (
-    <div className={styles.filterbar} onClick={() => setOpen((prev) => !prev)}>
-      <div className={styles.filterbar_label}>
-        <div className={`${styles.arrow}  ${open ? styles.active:''}`}>
+    <div className={styles.filterbar}>
+      <div
+        className={styles.filterbar_label}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        <div className={`${styles.arrow}  ${open ? styles.active : ""}`}>
           <KeyboardArrowDownIcon sx={{ marginTop: "5px" }} />
         </div>
         <p>Filter</p>
       </div>
       {open && (
         <div className={styles.hiddenBox}>
-          <div className={styles.selectBox}>
-            <div>
+          <div className={styles.Allselect}>
+            <div className={styles.select}>
+              <label for="room type">Room Type</label>
               <select name="room type" id="room type">
-                <option value="" selected disabled hidden />
+                <option value="all">All</option>
                 <option value="placeholder1">placeholder1</option>
                 <option value="placeholder1">placeholder1</option>
                 <option value="placeholder1">placeholder1</option>
               </select>
+            </div>
+
+            <div className={styles.select}>
               <label for="room type">Room Type</label>
+              <select name="room type" id="room type">
+                <option value="all">All</option>
+                <option value="placeholder1">placeholder1</option>
+                <option value="placeholder1">placeholder1</option>
+                <option value="placeholder1">placeholder1</option>
+              </select>
+            </div>
+
+            <div className={styles.select}>
+              <label for="room type">Room Type</label>
+              <select name="room type" id="room type">
+                <option value="all">All</option>
+                <option value="placeholder1">placeholder1</option>
+                <option value="placeholder1">placeholder1</option>
+                <option value="placeholder1">placeholder1</option>
+              </select>
+            </div>
+
+            <div className={styles.select}>
+              <label for="room type">Room Type</label>
+              <select name="room type" id="room type">
+                <option value="all">All</option>
+                <option value="placeholder1">placeholder1</option>
+                <option value="placeholder1">placeholder1</option>
+                <option value="placeholder1">placeholder1</option>
+              </select>
+            </div>
+
+            <div className={styles.select}>
+              <label for="room type">Room Type</label>
+              <select name="room type" id="room type">
+                <option value="all">All</option>
+                <option value="placeholder1">placeholder1</option>
+                <option value="placeholder1">placeholder1</option>
+                <option value="placeholder1">placeholder1</option>
+              </select>
             </div>
           </div>
           <div className={styles.actions}>
-            <button>Apply</button>
-            <button>Cancel</button>
+            <button type="submit">Apply</button>
+            <button type="submit">Reset</button>
           </div>
         </div>
       )}
